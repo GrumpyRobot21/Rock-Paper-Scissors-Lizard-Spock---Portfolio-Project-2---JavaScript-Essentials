fd961b0713f5fb81a8143451e6e11a1abef53846
Keithjmm <joenormal79@gmail.com>
2022-07-27 14:48:45 +0000

    cache DOM elments

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+// caching the DOM and connecting HTML elements with variables for easier debugging
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span =document.getElementById("computer-score");
+const results_p = document.querySelector(".results > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+const lizard_div = document.getElementById("l");
+const spock_div = document.getElementById("v");
+
+
