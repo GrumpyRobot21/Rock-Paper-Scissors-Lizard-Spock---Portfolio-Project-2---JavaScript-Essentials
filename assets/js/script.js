16e2de333f363b973844db890854350eebddcfdb
Keithjmm <joenormal79@gmail.com>
2022-07-29 09:41:46 +0000

    fix issue in vscode and update all element names to remove underscore. introduce score class to replace .results_p

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,204 +1,211 @@
-let userScore = 0;
-let computerScore = 0;
-const userScore_span = document.getElementById("user-score");
-const computerScore_span = document.getElementById("computer-score");
-const results_p = document.querySelector(".results > p");
-const rock_div = document.getElementById("r");
-const paper_div = document.getElementById("p");
-const scissors_div = document.getElementById("s");
-const lizard_div = document.getElementById("l");
-const spock_div = document.getElementById("v");
-
+// caching the DOM and connecting HTML elements with variables for easier debugging.
+let userScore = 0
+let computerScore = 0
+const userScoreSpan = document.getElementById("user-score")
+const computerScoreSpan = document.getElementById("computer-score")
+const resultsP = document.querySelector(".score")
+const rockDiv = document.getElementById("r")
+const paperDiv = document.getElementById("p")
+const scissorsDiv = document.getElementById("s")
+const lizardDiv = document.getElementById("l")
+const spockDiv = document.getElementById("v")
 
 /**
  * Random choice generator for computer player
  */
 function getComputerChoice() {
-    let choices = ["r", "p", "s", "l", "v"];
-    let randomNumber = Math.floor(Math.random() * 5);
-    return choices[randomNumber];
+  let choices = ["r", "p", "s", "l", "v"]
+  let randomNumber = Math.floor(Math.random() * 5)
+  return choices[randomNumber]
 }
 
 /**
  * converts user and computer choices into text for results display purposes
  */
 function convertToWord(letter) {
-    if (letter === "r") return "Rock";
-    if (letter === "p") return "Paper";
-    if (letter === "s") return "Scissors";
-    if (letter === "l") return "Lizard";
-    return "Spock";
+  if (letter === "r") return "Rock"
+  if (letter === "p") return "Paper"
+  if (letter === "s") return "Scissors"
+  if (letter === "l") return "Lizard"
+  return "Spock"
 }
 
 /**
  * Declares win for user when computer loses and amends user score total
  */
 function win(userChoice, computerChoice) {
-    userScore++;
+  userScore++
 
-    //Briefly changes background colour of user score div to indicate the win
-    changeCol();
+  // Briefly changes background color of user score div to indicate the win
 
-    function changeCol() {
-        document.getElementById("userback").style.background = "#1adbf4";
-        setTimeout(function () {
-            setNormal();
-        }, 100);
-    }
+  function changeCol() {
+    document.getElementById("userback").style.background = "#1adbf4"
+    setTimeout(function () {
+      setNormal()
+    }, 100)
+  }
+  changeCol();
 
-    function setNormal() {
-        document.getElementById("userback").style.background = "#2f54ce";
-    }
 
-    if (userScore === 10) {
-        endGame()
-      } else {
-        userScore_span.innerHTML = userScore
-        computerScore_span.innerHTML = computerScore //displays numerical score results for user and computer
-        results_p.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(
-          computerChoice
-        )}. You Win!!!`;
-      }
-    }
+
+  function setNormal() {
+    document.getElementById("userback").style.background = "#2f54ce"
+  }
+
+  if (userScore === 10) {
+    endgame()
+  } else {
+    userScoreSpan.innerHTML = userScore
+    computerScoreSpan.innerHTML = computerScore //displays numerical score results for user and computer
+    resultsP.innerHTML = `${convertToWord(userChoice)} beats ${convertToWord(
+      computerChoice
+    )}. You Win!!!`;
+  }
+}
 
 /**
  * Declares loss for user when computer wins and amends computer score total
  */
 function lose(userChoice, computerChoice) {
-    computerScore++;
+  computerScore++
 
-    // Briefly changes background colour of comp score div to indicate the win
-    setToBlue();
+  // Briefly changes background color of comp score div to indicate the win
+  setToBlue()
+  function setToBlue() {
+    document.getElementById("compback").style.background = "#1adbf4"
+    setTimeout(function () {
+      setNormal()
+    }, 100)
+  }
 
-    function setToBlue() {
-        document.getElementById("compback").style.background = "#1adbf4";
-        setTimeout(function () {
-            setNormal();
-        }, 100);
-    }
+  function setNormal() {
+    document.getElementById("compback").style.background = "#2f54ce"
+  }
 
-    function setNormal() {
-        document.getElementById("compback").style.background = "#2f54ce";
-    }
-
-    if (computerScore === 10) {
-        endGame()
-      } else {
-        userScore_span.innerHTML = userScore
-        computerScore_span.innerHTML = computerScore //displays numerical score results for user and computer
-        results_p.innerHTML = `${convertToWord(
-          userChoice
-        )} loses to ${convertToWord(computerChoice)}. You Lost...`;
-      }
-    }
+  if (computerScore === 10) {
+    endgame()
+  } else {
+    userScoreSpan.innerHTML = userScore
+    computerScoreSpan.innerHTML = computerScore //displays numerical score results for user and computer
+    resultsP.innerHTML = `${convertToWord(
+      userChoice
+    )} loses to ${convertToWord(computerChoice)}. You Lost...`;
+  }
+}
 
 /**
  * Declares a draw if user and computer make same choice
  */
 function draw(userChoice, computerChoice) {
-    results_p.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(computerChoice)}. It's a Draw!!`;
+  resultsP.innerHTML = `${convertToWord(userChoice)} equals ${convertToWord(
+    computerChoice
+  )}. It's a Draw!!`
 }
 
 /**
- * Declares winner after 10 rounds then resets game
+ * Declares winner after 10 rounds then resets game.
  */
-function endGame(win, lose) {
-    if (computerScore === 10) {
+function endgame(win, lose) {
+  if (computerScore === 10) {
+    let element = document.querySelector(".results > p")
+    element.style.backgroundColor = "red" // changes result output background-colour
 
-        //changes result output background colour
-        let element = document.querySelector(".results_p");
-        element.style.backgroundColor = "red";
+    resultsP.innerHTML = `Bad luck, the computer won ${computerScore} rounds. You lost the game!!! (The Game will restart shortly. Have another go!).`
 
-        //announces computer win and loss for player
-        results_p.innerHTML = `Bad luck, the computer won ${computerScore} rounds. You lost the game!!! (The Game will restart shortly. Have another go!).`;
+    // clears icon images from gameplay area
+    let clear = document.querySelector(".choices")
+    clear.remove()
 
-        //Clears icon images from gameplay area
-        let clear = document.querySelector(".choices");
-        clear.remove();
+    //plays short audio file to signify game lost
+    let loseSound = new Audio(
+      "/assets/audio/loseSound.wav"
+    )
+    loseSound.play()
+  } else if (userScore === 10) {
+    let element = document.querySelector(".score")
+    element.style.backgroundColor = "green" // changes result output background-colour
 
-        //plays short audio clip to signify game lost
-        let loseSound = new Audio("assets/audio/loseSound.wav");
-        loseSound.play();
-    } else if (userScore === 10) {
+    resultsP.innerHTML = `Well done, you won ${userScore} rounds. Live long and prosper!!! (The Game will restart shortly. Have another go!).`
+    // clears icon images from gameplay area
+    let clear = document.querySelector(".choices")
+    clear.remove()
 
-        //changes result output background colour
-        let element = document.querySelector(".results_p");
-        element.style.backgroundColor = "green";
+    //plays short audio file to signify game won
+    let winSound = new Audio(
+      "/assets/audio/winSound.wav"
+    )
+    winSound.play()
+  }
 
-        //announces win for player
-        results_p.innerHTML = `Well done, you won ${userScore} rounds. Live long and prosper!!! (The Game will restart shortly. Have another go!).`;
-
-        //plays short audio clip to signify game won
-        let winSound = new Audio("assets/audio/winSound.wav");
-        winSound.play();
-
-        //Resets game after 7 seconds once winner is declared
-        setTimeout(function () {
-            location.reload();
-        }, 7000);
-    }
+  //resets game after 7 seconds once winner is declared
+  setTimeout(function () {
+    location.reload()
+  }, 7000)
 }
 
 /**
  * Defines winning scenarios, losing scenarios and draw scenarios
  */
-
 function playGame(userChoice) {
-    const computerChoice = getComputerChoice();
-    switch (userChoice + computerChoice) {
-        case "rs":
-        case "pr":
-        case "sp":
-        case "rl":
-        case "lv":
-        case "vs":
-        case "sl":
-        case "lp":
-        case "pv":
-        case "vr":
-            win(userChoice, computerChoice);
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-        case "lr":
-        case "vl":
-        case "sv":
-        case "ls":
-        case "pl":
-        case "vp":
-        case "rv":
-            lose(userChoice, computerChoice);
-            break;
-        case "rr":
-        case "pp":
-        case "ss":
-        case "ll":
-        case "vv":
-            draw(userChoice, computerChoice);
-            break;
-    }
+  const computerChoice = getComputerChoice()
+  switch (userChoice + computerChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+    case "rl":
+    case "lv":
+    case "vs":
+    case "sl":
+    case "lp":
+    case "pv":
+    case "vr":
+      win(userChoice, computerChoice)
+      break
+    case "rp":
+    case "ps":
+    case "sr":
+    case "lr":
+    case "vl":
+    case "sv":
+    case "ls":
+    case "pl":
+    case "vp":
+    case "rv":
+      lose(userChoice, computerChoice)
+      break
+    case "rr":
+    case "pp":
+    case "ss":
+    case "ll":
+    case "vv":
+      draw(userChoice, computerChoice)
+      break
+  }
 }
 
 /**
- * Gameplay event listeners for user icon choices
+ * Gameplay event listeners for player icon choices
  */
 function main() {
-    rock_div.addEventListener("click", function () {
-        playGame("r");
-    });
-    paper_div.addEventListener("click", function () {
-        playGame("p");
-    });
-    scissors_div.addEventListener("click", function () {
-        playGame("s");
-    });
-    lizard_div.addEventListener("click", function () {
-        playGame("l");
-    });
-    spock_div.addEventListener("click", function () {
-        playGame("v");
-    });
+  rockDiv.addEventListener("click", function () {
+    playGame("r")
+  })
+
+  paperDiv.addEventListener("click", function () {
+    playGame("p")
+  })
+
+  scissorsDiv.addEventListener("click", function () {
+    playGame("s")
+  })
+
+  lizardDiv.addEventListener("click", function () {
+    playGame("l")
+  })
+
+  spockDiv.addEventListener("click", function () {
+    playGame("v")
+  })
 }
 
-main();
+main()
